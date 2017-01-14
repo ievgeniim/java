1d3330ba1b9a392ce0e264bd20502dc8007a21e9
pc\lagger <yevheniy.martynenko@gmail.com>
2017-01-14 18:36:28 +0200

    Spark update

--- Spark/src/main/java/Record.java
+++ Spark/src/main/java/Record.java
@@ -1,7 +1,21 @@
-package PACKAGE_NAME;
+import java.io.Serializable;
 
 /**
  * Created by lagger on 12.01.2017.
  */
-public class Record {
+public class Record implements Serializable {
+    int airportId;
+    String name;
+    String city;
+    String country;
+    String IATA;
+    String ICAO;
+    float lattitude;
+    float longtitude;
+    float attitude;
+    float timezone;
+    String dst;
+    String timezoneTz;
+    String type;
+    String source;
 }
